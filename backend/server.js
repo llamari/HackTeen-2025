4c214edf0602cd9c9412cf994d176b3e75bf54c1
llamari <saralamarisilva@gmail.com>
2026-01-25 20:44:20 -0300

    fix: fixing minor issues

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,11 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import cors from 'cors';
 const app = express();
-const port = 5000;
 
 import usersRoutes from './src/routes/usersRoutes.js';
 import ttsRoutes from './src/routes/ttsRoutes.js';
 import SwaggerImport from './swagger.json' with {type: 'json'}
-import { sequelize } from './src/database/database.js';
 import { } from './src/database/connection.js';
-
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(SwaggerImport));
 
@@ -27,15 +24,6 @@ app.use(cors({
 }));
 
 app.use(express.json());
-
-(async () => {
-  try {
-    await sequelize.sync();
-    console.log('Banco sincronizado com sucesso!');
-  } catch (error) {
-    console.error('Erro ao sincronizar o banco:', error);
-  }
-})();
 
 app.get('/', (req, res) => res.send("API do Inclusound rodando!"))
 
